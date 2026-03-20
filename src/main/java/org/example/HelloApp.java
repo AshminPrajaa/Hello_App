773df66ea4bb71bb6abb225eb48a8a69591021e4
AshminPrajaa <ashminprajaa27@gmail.com>
2026-03-20 10:41:11 +0530

    use case 1 display hello

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class HelloApp {
-    public static void main(String[] args){
+     public static void main(String[] args){
         System.out.println("Hello World!");
     }
 }
