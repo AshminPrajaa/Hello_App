ce652fda7fd9dbacc91564b642f22f6e36419b39
AshminPrajaa <ashminprajaa27@gmail.com>
2026-03-20 10:43:02 +0530

    use case 3

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -1,7 +1,14 @@
 package org.example;
 
 public class HelloApp {
-     public static void main(String[] args){
-        System.out.println("Hello World!");
+    public static void main(String[] args){
+        String name = "World";
+        if (args.length > 0){
+            name=args[0];
+            System.out.println("Hello,"+ name + "!");
+        } else {
+            System.out.println("java HelloApp Alice!");
+        }
+
     }
 }
